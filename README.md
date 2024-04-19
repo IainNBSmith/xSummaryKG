6d486704efd5daf9312549b67b6df420e0d08ab4
Iain Smith <iainnbsmith@gmail.com>
2024-04-19 12:26:00 -0600

    Add final requirements
    -Link required packages
    -Add source data
    -Modify comments
    -Correct usage file

--- README.md
+++ README.md
@@ -5,19 +5,31 @@ Compare information stored in summary and original articles to evaluate summariz
 
 Requirements:
 
-XTE          - (Entailment)
-StanfordNLP  - (Open Information Extraction)
-WS4J         - (Semantic Similarity)
+
+[XTE](https://github.com/ssvivian/XTE)                 - (Entailment)
+
+[StanfordNLP OpenIE](https://nlp.stanford.edu/software/openie.html)  - (Open Information Extraction)
+
+[WS4J](https://github.com/dmeoli/WS4J)                - (Semantic Similarity)
 
 
 Usage:
+
+To use load all required libraries in preferred IDE (IntellijIdea recommended). Run the files summary/src/Main.java
+
+Replace the named files in main function to change the compared files. Outputs all the relations from both sources, missing 
+relationships, precision, recall, and relation ratio.
 
 
 
 
 Articles Used:
 
+
 [dailyshow](https://www.washingtonpost.com/entertainment/tv/2024/01/24/jon-stewart-return-daily-show/) - Washington Post
+
 [hockey](https://www.stonyplainreporter.com/news/local-news/kids-helping-kids-worlds-longest-hockey-game-juniors-raises-400k-for-paediatric-cancer-research)    - Stony Plain Reporter
+
 [gunn](https://www.stonyplainreporter.com/news/local-news/gunn-man-woman-charged)      - Stony Plain Reporter
+
 [trump](https://www.stonyplainreporter.com/entertainment/celebrity/trump-allies-vow-holy-war-against-taylor-swift-over-biden-endorsement)     - Stony Plain Reporter
--- summary/src/Main.java
+++ summary/src/Main.java
@@ -1,4 +1,5 @@
-// Code sample from Standford NLP toolkit
+// Adapted from code samples from Standford NLP toolkit
+// XTE settings are from the XTE packages usage
 import com.sun.xml.bind.api.impl.NameConverter;
 import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.ling.*;
@@ -39,6 +40,7 @@ import edu.uniba.di.lacam.kdde.ws4j.RelatednessCalculator;
 
 public class Main {
 
+    // XTE settings
     private static String configFile = "../../NLP/XTE-master/config.txt"; //the configuration file path
     private static String kb = "WN"; //WN (WordNet), WKT (Wikitionary), WKP (Wikipedia) or WBT (Webster's)
     private static Entailment entail = new Entailment(kb, configFile);
@@ -248,7 +250,7 @@ public class Main {
     public static Collection<RelationTriple> extractRelations(String docName) {
         // get all the relations in a valid format in one collection
         String content = "default"; // working so far
-        // requires exception handling here
+        // requires exception handling for opening file
         try {
             content = new String(Files.readString(Paths.get("../Data/" + docName), Charset.defaultCharset()));
             System.out.println(content);
@@ -267,7 +269,6 @@ public class Main {
 
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
         // create a document object
-        // CoreDocument document = new CoreDocument(content);
         Annotation document = new Annotation(content);
         // annnotate the document
         pipeline.annotate(document);
@@ -296,19 +297,16 @@ public class Main {
                                             otriple.objectLemmaGloss().length() +
                                             otriple.relationLemmaGloss().length() ) )
                             ) {
-                                same = true; // change to true
+                                same = true;
                             }
                         }
                     }
                 } else {
-                    //System.out.println("trimmed");
+                    //remove current relation
                     trimmed = true;
                 }
-                //if (same) {
-                //    System.out.println("matched");
-                //}
                 if (!trimmed && !same) {
-                    // put it into final file, for now just count
+                    // put it into final file
                     finalTriples.add(triple);
                 }
             }
@@ -316,198 +314,11 @@ public class Main {
         return finalTriples;
     }
 
-    //public static String text = "Joe Smith was born in California. In 2017, he went to Paris, France in the summer. His flight left at 3:00pm on July 10th, 2017. After eating some escargot for the first time, Joe said, \"That was delicious!\" He sent a postcard to his sister Jane Smith. After hearing about Joe's trip, Jane decided she might go to France one day.";
-    // add just extracting entity relations
     public static void main(String[] args) {
-        Collection<RelationTriple> triplesInit = extractRelations("dailyshow.txt");
-        Collection<RelationTriple> triplesSum = extractRelations("dailyshow-copilot-noprompt.txt");
+        // articles in Data folder, change names for different comparisons
+        Collection<RelationTriple> triplesInit = extractRelations("trumpUnproblematic.txt");
+        Collection<RelationTriple> triplesSum = extractRelations("trumpUnproblematic-copilot-noprompt.txt");
 
         compareTripleSets(triplesInit, triplesSum);
     }
 }
-
-        /*
-        String content = "default"; // working so far
-        // requires exception handling here
-        try {
-            content = new String(Files.readString(Paths.get("../Data/dailyshow.txt"), Charset.defaultCharset()));
-            System.out.println(content);
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        // set up pipeline properties
-        Properties props = new Properties();
-        // set the list of annotators to run
-        // coref for correference annotation
-        // go through triples and resolve corefferences
-        // wonder how I should have this done
-        props.setProperty("annotators", "tokenize,pos,lemma,ner,depparse,natlog,coref,openie");
-        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
-        props.setProperty("ner.rulesOnly", "true"); // only use rules no statistics
-        props.setProperty("coref.algorithm", "statistical"); // only this or neural and this is a bit better
-        props.setProperty("openie.resolve_coref", "true"); // coref requirement is missing in error message
-        // build pipeline
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        // create a document object
-        // CoreDocument document = new CoreDocument(content);
-        Annotation document = new Annotation(content);
-        // annnotate the document
-        pipeline.annotate(document);
-
-        /*
-        System.out.println("---");
-        System.out.println("coref chains");
-        for (CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
-            System.out.println("\t" + cc);
-        }
-        for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
-            System.out.println("---");
-            System.out.println("mentions");
-            for (Mention m : sentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class)) {
-                System.out.println("\t" + m);
-            }
-        }
-        */
-        /*
-        // triples
-        // need to do named entity recognition and disambiguation
-        int ncount = 0;
-        for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
-            // Get the OpenIE triples for the sentence
-            Collection<RelationTriple> triples =
-                    sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
-            // Print the triples
-            for (RelationTriple triple : triples) {
-                System.out.println(triple.confidence + "\t" +
-                        triple.subjectLemmaGloss() + "\t" +
-                        triple.relationLemmaGloss() + "\t" +
-                        triple.objectLemmaGloss());
-                Boolean same = false;
-                Boolean trimmed = false;
-                if (checkTripleValid(triple)) {
-                    for (RelationTriple otriple : triples) {
-                        if (same) {
-                            break;
-                        }
-                        if (checkTripleValid(otriple) && otriple != triple) {
-                            if (new String(otriple.subjectLemmaGloss()).contains(triple.subjectLemmaGloss()) &&
-                                    new String(otriple.relationLemmaGloss()).contains(triple.relationLemmaGloss()) &&
-                                    new String(otriple.objectLemmaGloss()).contains(triple.objectLemmaGloss())) {
-                                same = true;
-                            }
-                        }
-                    }
-                } else {
-                    System.out.println("trimmed");
-                    trimmed = true;
-                }
-                if (same) {
-                    System.out.println("matched");
-                }
-                if (!trimmed && !same) {
-                 // put it into final file, for now just count
-                 ncount += 1;
-                }
-                }
-            }
-        System.out.println(ncount);
-        }
-        */
-        //Map<Integer, CorefChain> corefChains = document.CorefChains();
-        //System.out.println("Example: coref chains for document");
-        //System.out.println(corefChains);
-        //System.out.println();
-
-        // examples
-/*
-        // 10th token of the document
-        CoreLabel token = document.tokens().get(3);
-        System.out.println("Example: token");
-        System.out.println(token);
-        System.out.println();
-
-        // text of the first sentence
-        String sentenceText = document.sentences().get(0).text();
-        System.out.println("Example: sentence");
-        System.out.println(sentenceText);
-        System.out.println();
-
-        // second sentence
-        CoreSentence sentence = document.sentences().get(1);
-
-        // list of the part-of-speech tags for the second sentence
-        List<java.lang.String> posTags = sentence.posTags();
-        System.out.println("Example: pos tags");
-        System.out.println(posTags);
-        System.out.println();
-
-        // list of the ner tags for the second sentence
-        List<String> nerTags = sentence.nerTags();
-        System.out.println("Example: ner tags");
-        System.out.println(nerTags);
-        System.out.println();
-
-        // constituency parse for the second sentence
-        Tree constituencyParse = sentence.constituencyParse();
-        System.out.println("Example: constituency parse");
-        System.out.println(constituencyParse);
-        System.out.println();
-
-        // dependency parse for the second sentence
-        SemanticGraph dependencyParse = sentence.dependencyParse();
-        System.out.println("Example: dependency parse");
-        System.out.println(dependencyParse);
-        System.out.println();
-
-        /*
-        // kbp relations found in fifth sentence
-        List<RelationTriple> relations =
-                document.sentences().get(11).relations();
-        System.out.println("Example: relation");
-        System.out.println(relations.get(0));
-        System.out.println(relations.get(1));
-        System.out.println();
-
-         */
-/*
-        // entity mentions in the second sentence
-        List<CoreEntityMention> entityMentions = sentence.entityMentions();
-        System.out.println("Example: entity mentions");
-        System.out.println(entityMentions);
-        System.out.println();
-
-        // coreference between entity mentions
-        CoreEntityMention originalEntityMention = document.sentences().get(0).entityMentions().get(1);
-        System.out.println("Example: original entity mention");
-        System.out.println(originalEntityMention);
-        System.out.println("Example: canonical entity mention");
-        System.out.println(originalEntityMention.canonicalEntityMention().get());
-        System.out.println();
-
-        // get document wide coref info
-        // coreference resolution
-        Map<Integer, CorefChain> corefChains = document.corefChains();
-        System.out.println("Example: coref chains for document");
-        System.out.println(corefChains);
-        System.out.println();
-
-        // get quotes in document
-        List<CoreQuote> quotes = document.quotes();
-        CoreQuote quote = quotes.get(0);
-        System.out.println("Example: quote");
-        System.out.println(quote);
-        System.out.println();
-
-        // original speaker of quote
-        // note that quote.speaker() returns an Optional
-        System.out.println("Example: original speaker of quote");
-        System.out.println(quote.speaker().get());
-        System.out.println();
-
-        // canonical speaker of quote
-        System.out.println("Example: canonical speaker of quote");
-        System.out.println(quote.canonicalSpeaker().get());
-        System.out.println();
-*/
